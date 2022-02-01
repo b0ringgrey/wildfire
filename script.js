9d8bb5a666c8376d6dfe1167480b81d5d526c6d6
b0ringgrey <98797227+b0ringgrey@users.noreply.github.com>
2022-01-31 22:05:20 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const Form = () => {
   const [state, setState] = React.useState({
     bombOne:
-    "https://pbs.twimg.com/profile_images/1484628496148099072/ljIIjeFo_400x400.jpg",
+    "https://lh3.googleusercontent.com/fpxkW42dJqikW4Ed9w0ubqF4QHBsh7jddWeXRzH1OKTubnEYd0ByrvLiXB_7F0xQ3L5lROpxdoUmTJpK2S7r2Au7XSuyQjU7PwG5Qt8=s0",
     bombTwo:
-    "https://pbs.twimg.com/profile_images/1484236379227779072/hLqwg3YI_400x400.jpg",
+    "https://lh3.googleusercontent.com/pksyaB2SBcYZfxvpoOMJA263y3dLSMN55rBvE_TpJvfdzDzvJisNjuZ1li6vH2iOKqdtVU6hUhIRqkyFyVTGyWy_RNYlE-ZUABYr5w=s0",
     bombThree:
-    "https://lh3.googleusercontent.com/C5ubcUTfHNyqJT9vEdLhk3WJ3enHQn54OhRNu2sciw5tVzbIoJHoj9CrfwziGFhdNCwroSvlFvpT5hUnA4hrl52Pprm6R-VWs48ysQ=w600" });
+    "https://lh3.googleusercontent.com/xFsStH9iu1ApdqWe7mSSD0lr9egP3MsorDk8idI7Y5VXDoqvPhb7a_1KBWvdgCvfm_vStUwBGMmgJdF6Mm8GOdHmeQrG-GbqtZnq=s0" });
 
 
   const submitHandler = e => {
@@ -21,10 +21,35 @@ const Form = () => {
 
   return /*#__PURE__*/(
     React.createElement("div", null, /*#__PURE__*/
+    React.createElement("h1", null, "ABS x Wildfire"), /*#__PURE__*/
+    React.createElement("h2", null, "by ", /*#__PURE__*/
+    React.createElement("a", { href: "https://opensea.io/b0ring_gray" }, "boringgrey"), " (", /*#__PURE__*/
+    React.createElement("a", { href: "https://twitter.com/BoringGrey" }, "twitter"), ")"), /*#__PURE__*/
+
+    React.createElement("h3", null, /*#__PURE__*/
+    React.createElement("a", { href: "https://thehundreds.com/blogs/content/firestarters-story-behind-wildfire-flag" }, "The story behind Wildfire")), /*#__PURE__*/
+
+
+
+    React.createElement("h3", null, "Copy the image address of your Bomb and paste it into the fields below and generate a Wildfire collage of your bomb, then take a screenshot."), /*#__PURE__*/
+
+
+
     React.createElement("form", null, /*#__PURE__*/
-    React.createElement("input", { onChange: changeHandler, type: "text", name: "bombOne" }), /*#__PURE__*/
-    React.createElement("input", { onChange: changeHandler, type: "text", name: "bombTwo" }), /*#__PURE__*/
-    React.createElement("input", { onChange: changeHandler, type: "text", name: "bombThree" })), /*#__PURE__*/
+    React.createElement("div", { class: "container" }, /*#__PURE__*/
+    React.createElement("div", { class: "item" }, /*#__PURE__*/
+    React.createElement("label", { className: "label" }, "image url 1"), /*#__PURE__*/
+    React.createElement("input", { onChange: changeHandler, type: "text", name: "bombOne" })), /*#__PURE__*/
+
+    React.createElement("div", { class: "item" }, /*#__PURE__*/
+    React.createElement("label", { className: "label" }, "image url 2"), /*#__PURE__*/
+    React.createElement("input", { onChange: changeHandler, type: "text", name: "bombTwo" })), /*#__PURE__*/
+
+    React.createElement("div", { class: "item" }, /*#__PURE__*/
+    React.createElement("label", { className: "label" }, "image url 3"), /*#__PURE__*/
+    React.createElement("input", { onChange: changeHandler, type: "text", name: "bombThree" })))), /*#__PURE__*/
+
+
 
     React.createElement("div", { class: "pfp" }, /*#__PURE__*/
     React.createElement("div", {
